1f18533c75ec73b16f4305b03b5c303d95f78475
chenll <chenll@lieluobo.com>
2019-03-12 09:36:55 +0800

    add wesocket

--- eureka-client-consumer/src/main/java/hello/ClientConsumerApplication.java
+++ eureka-client-consumer/src/main/java/hello/ClientConsumerApplication.java
@@ -1,18 +1,10 @@
 package hello;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 
 @EnableDiscoveryClient
@@ -26,38 +18,4 @@ public class ClientConsumerApplication {
     SpringApplication.run(ClientConsumerApplication.class, args);
   }
 
-/*
-  static final String topicExchangeName = "spring-boot-exchange";
-
-  static final String queueName = "spring-boot";
-
-  @Bean
-  Queue queue() {
-    return new Queue(queueName, false);
-  }
-
-  @Bean
-  TopicExchange exchange() {
-    return new TopicExchange(topicExchangeName);
-  }
-
-  @Bean
-  Binding binding(Queue queue, TopicExchange exchange) {
-    return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
-  }
-
-  @Bean
-  SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-                                           MessageListenerAdapter listenerAdapter) {
-    SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-    container.setConnectionFactory(connectionFactory);
-    container.setQueueNames(queueName);
-    container.setMessageListener(listenerAdapter);
-    return container;
-  }
-
-  @Bean
-  MessageListenerAdapter listenerAdapter(Receiver receiver) {
-    return new MessageListenerAdapter(receiver, "receiveMessage");
-  }*/
 }
